0feb22af09d5239b8e4f6ecc2d554342c352e3d9
Andrii Malnov <andrey.malnov@gmail.com>
2021-08-04 10:27:59 +0300

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Mac on 04.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
